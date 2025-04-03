9dd21b31d023b04c827a7e704b330f7a28571296
Tomas Casas <tomas.casas2001@gmail.com>
2025-04-03 10:54:49 -0300

    role validation

--- src/Routes/auth.routes.js
+++ src/Routes/auth.routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const {register, login} = require("../Controllers/auth.Controller");
+const authenticate = require("../middlewares/auth");
+
 
 router.post("/register", register);
 router.post("/login", login);
 
+router.get("/profile", authenticate, (req, res) => {
+  const { id, role } = req.user;
+  res.json({ id, role });
+});
 module.exports = router;
